cf0bbd6ca72d2594d30c305965c5ccffb44b4fc6
Yadhukrishnx <Yadhukrishnx@gmail.com>
2024-05-19 13:35:38 +0530

    Create : Form to Database

--- newapp/models.py
+++ newapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title=models.CharField(max_length=20)
+    price=models.IntegerField()
+    
+    def __str__(self):
+        return '{}'.format(self.title)
+    
--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+
+def createbook(request):
+    if request.method=='POST':
+        title=request.POST.get('title')
+        price=request.POST.get('price')
+        
+        book=Book(title=title,price=price)
+        
+        book.save()
+    
+    return render(request,'index.html')
