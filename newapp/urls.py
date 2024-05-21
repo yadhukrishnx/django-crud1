791d71ac0c8ac715b3781c3354a36cb90e768846
Yadhukrishnx <Yadhukrishnx@gmail.com>
2024-05-21 18:42:24 +0530

    CRUD : Optimized

--- newapp/urls.py
+++ newapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("",views.createbook),
-    path("detailedview/<int:book_id>/",views.detailedview),
-    path("updatebook/<int:book_id>/",views.updatebook),
+    path("detailedview/<int:book_id>/",views.detailedview,name='details'),
+    path("updatebook/<int:book_id>/",views.updatebook,name='update'),
 
-    path("deletebook/<int:book_id>/",views.deletebook),
+    path("deletebook/<int:book_id>/",views.deletebook,name='delete'),
 ]
